a60fd69dbc7324bec3e180582abbf05d7e4f8b89
satvshr <112589278+satvshr@users.noreply.github.com>
2023-01-14 01:54:05 +0530

    3rd commit

--- pq5/pq5(1).py
+++ pq5/pq5(1).py
@@ -29,25 +29,23 @@ def isValidChessBoard(board):
             return False        
         if sum(i == "brook" for i in board.values()) > 2 or sum(i == "wrook" for i in board.values()) > 2:
             return False
-
-        # check for a valid location
-        square = list(board)  # create list of dictionary keys. eg: ['1h', '6c', '2g', '5h', '3e']
+        square = list(board)  
+        
         y = ["1", "2", "3", "4", "5", "6", "7", "8"]
-        vertical = [s[0] for s in square]  # removes letters from list. eg: ['1', '6', '2', '5', '3']
-        if not all(i in y for i in vertical):  # checks if all values from vertical are in the y-list
+        vertical = [s[0] for s in square]  
+        if not all(i in y for i in vertical):
             return False
 
         x = ["a", "b", "c", "d", "e", "f", "g", "h"]
-        horiz = [s[1] for s in square]  # removes numbers from list. eg: ['h', 'c', 'g', 'h', 'e']
-        if not all(i in x for i in horiz):  # checks if all values from horiz are in the x-list
+        horiz = [s[1] for s in square]  
+        if not all(i in x for i in horiz):  
             return False
 
-        # check if the name starts with a "w" or "b"
+        
         for i in board.values():
             if i[0] != "b" and i[0] != "w":
                 return False
 
-        # check if the pieces_names name is valid
         pieces_names = ["pawn", "knight", "bishop", "rook", "queen", "king"]
         for i in board.values():
             if i[1:] not in pieces_names:
@@ -55,8 +53,7 @@ def isValidChessBoard(board):
         return True
 
 
-# testing boards
 print(isValidChessBoard({"1h": "bking", "6c": "wqueen", "2g": "bbishop", "5h": "bqueen", "3e": "wking"}))  # True
-print(isValidChessBoard({"1a": "bpawn", "2a": "wking"}))  # False: no bking
-print(isValidChessBoard({"1a": "wking", "2a": "wking", "3c": "bbishop"}))  # False: cannot have 2 white kings, no bking
-print(isValidChessBoard({"1a": "bking", "9z": "wking"}))  # False: 9z is an invalid position
+print(isValidChessBoard({"8h": "bbishop", "5k": "bking", "4f":"frook"}))  
+print(isValidChessBoard({"4b": "wking", "3d": "wking", "3c": "bbishop"}))  #program doesnt check if 2 kings are side by side, soemthing which is not possible in actual chess
+print(isValidChessBoard({"1a": "bking", "9z": "wking"}))  
