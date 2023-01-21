e6c008f328984fc562ffe308be898221886dc5c8
satvshr <112589278+satvshr@users.noreply.github.com>
2023-01-21 23:23:25 +0530

    test

--- pq10/fillingaps.py
+++ pq10/fillingaps.py
@@ -1,6 +1,17 @@
 import re, os
-object = re.compile(r'00')
-dirlist = os.listdir('/home/satvshr/amfoss-tasks-2/pq9/testfolder')
+dirlist = os.listdir(C:\Users\satvm\OneDrive\Desktop\amfoss-tasks-2\pq10\gapsfill')
+total = 0
+dir = []
 for i in dirlist:
-    mo = object.search(f'{i}')     
-    print(mo)
+    total += 1
+    dir.append(i)
+
+object = re.compile(r'00[0-total]')
+for file in dirlist:
+    mo = onject.search(file)               
+    print(file)
+    
+
+
+
+
