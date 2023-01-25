f800f61caafa792b8e4cfb529b855a7ee34e52ab
satvshr <satvmishi@gmail.com>
2023-01-26 01:42:08 +0530

    pq13

--- p.py
+++ p.py
@@ -1,18 +1,42 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import random
+import openpyxl, sys
+if len(sys.argv) == 4:
+    n = int(sys.argv[1])
+    m = int(sys.argv[2])
+    doc = sys.argv[3]
+    print(sys.argv)
+
+    open = openpyxl.load_workbook(doc)
+    sheet = open['Sheet']
+    sheet1 = open['Sheet1']
+    col_len = 0
+    row_len = 0
+
+    for i in range(1, 1000):
+        if sheet.cell(row = i, column = 1).value != None:
+            row_len += 1
+        else:
+            break
+    print(row_len)
+    for i in range(1, 1000):
+        if sheet.cell(row = 1, column = i).value != None:
+            col_len += 1
+        else:
+            break
+    print(col_len)
+    for i in range(1, n+1):
+        for j in range(1, col_len+1):
+            sheet1.cell(row = i, column = j).value = sheet.cell(row = i, column = j).value
+            # insert all elements till nth row
+
+    for i in range(n+m+1, row_len+m+1): #, n+m+1+row_len-n #n+m+1, row_len+m+1
+        for j in range(1, col_len+1):
+            sheet1.cell(row = i, column = j).value = sheet.cell(row = i-m, column = j).value
+            # insert elements from (n+m)th row till ((n+m) + m)th row
+
+    for i in range(n+1, n+m+1):
+        for j in range(1, col_len+1):
+            sheet1.cell(row = i, column = j).value = None
+    open.save(doc)
 
 
-driver = webdriver.Firefox(executable_path='/Users/keneudeh/Downloads/geckodriver')
-driver.get('https://play2048.co/')
 
-key_select = [Keys.UP, Keys.DOWN, Keys.LEFT, Keys.RIGHT]
-
-gameStatusElem = driver.find_element_by_css_selector('.game-container p')
-htmlElem = driver.find_element_by_css_selector('html')
-
-
-while gameStatusElem.text != 'Game over!':
-    
-    htmlElem.send_keys(key_select[random.randint(0, 3)])
-    gameStatusElem = driver.find_element_by_css_selector('.game-container p')
