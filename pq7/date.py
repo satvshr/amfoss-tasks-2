57c7936c511256a48b5f417ebfafb69cd64da044
satvshr <satvmishi@gmail.com>
2023-01-27 02:16:58 +0530

    fixed date

--- pq7/date.py
+++ pq7/date.py
@@ -1,10 +1,12 @@
 def leap(n):
+
     if n % 4 == 0:
-        if n % 100 == 0:
-            if n % 400 == 0:
-                return True
+        return True
+    elif n % 100 == 0 and n % 400 == 0:
+        return True
     else:
         return False
+
 while True:
     import re
     date = re.compile(r'(\d\d)/(\d\d)/(\d\d\d\d)') 
@@ -16,25 +18,24 @@ while True:
         day.replace(day[0],'')
     if month[0] == "0":
         month.replace(month[0],'')
-    year = mo.group(3)
+    year = int(mo.group(3))
     print(day, month, year)
     if year not in range(1000, 3000):
         print("1")
         print("Enter a valid date.")
-    elif month not in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"):
+    elif month not in ("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"):
+        print(month)
         print("2")
         print("Enter a valid date.")
-    elif day == "31" and month not in ("1", "3", "5", "7", "8", "10", "12"):
+    elif day == "31" and month not in ("01", "03", "05", "07", "08", "10", "12"):
         print("Enter a valid date.")
-    elif day == "30" and month not in ("4", "6", "9", "11"): 
+    elif day == "30" and month not in ("04", "06", "09", "11"): 
         print("Enter a valid date.")
-    elif leap(year) == True and day == "29" and month in ("2"):
+    elif leap(year) == True and day == "29" and month in ("02"):
         print(f"Date you entered: {mo.group(0)} is valid")
         break
-    elif leap(year) == False and day == "28" and month in ("2"):
+    elif day == "28" and month in ("02"):
         print(f"Date you entered: {mo.group(0)} is valid")
         break
     else:
-        print(f"Date you entered: {mo.group(0)} is valid")
-        break
-
+        print(f"Date you entered: {mo.group(0)} is not valid")
